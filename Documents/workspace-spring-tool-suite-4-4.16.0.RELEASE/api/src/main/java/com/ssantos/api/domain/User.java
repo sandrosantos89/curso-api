4c5af23694b12de235df79db05330302a23ec671
Sandro Santos <sandro.ads.dev@gmail.com>
2022-10-19 21:16:00 -0300

    Primeiro Resource

--- Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/domain/User.java
+++ Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/domain/User.java
@@ -1,24 +1,13 @@
 package com.ssantos.api.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
-@ToString
 @Entity
 public class User {
 
@@ -30,5 +19,66 @@ public class User {
 	@Column(unique = true)
 	private String email;
 	private String password;
+
+	public User() {
+	}
+
+	public User(Integer id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password);
+	}
 
 }
