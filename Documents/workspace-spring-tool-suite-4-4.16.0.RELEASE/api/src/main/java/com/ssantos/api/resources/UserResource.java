40015db2cac29ddeca274e54794b5f557ae7816e
Sandro Santos <sandro.ads.dev@gmail.com>
2022-10-21 17:28:01 -0300

    Bean

--- Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/resources/UserResource.java
+++ Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.ssantos.api.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,21 +8,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssantos.api.domain.Users;
+import com.ssantos.api.domain.dto.UserDTO;
 import com.ssantos.api.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
+	@Autowired
+	private ModelMapper mapper;
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Users> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(service.findById(id));
-		
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+
 	}
-	
 
 }
