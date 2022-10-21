5f7ff30f15d96583d151d22583f74ae64df0287f
Sandro Santos <sandro.ads.dev@gmail.com>
2022-10-21 17:46:03 -0300

    findAll

--- Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/resources/UserResource.java
+++ Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/resources/UserResource.java
@@ -1,5 +1,10 @@
 package com.ssantos.api.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.apache.catalina.connector.Response;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssantos.api.domain.Users;
 import com.ssantos.api.domain.dto.UserDTO;
 import com.ssantos.api.services.UserService;
 
@@ -24,7 +30,11 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
-
 	}
 
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		return ResponseEntity.ok()
+				.body(service.findAll().stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
+	}
 }
--- Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/services/UserService.java
+++ Documents/workspace-spring-tool-suite-4-4.16.0.RELEASE/api/src/main/java/com/ssantos/api/services/UserService.java
@@ -1,8 +1,12 @@
 package com.ssantos.api.services;
 
+import java.util.List;
+
 import com.ssantos.api.domain.Users;
 
 public interface UserService {
 
 	Users findById(Integer id);
+
+	List<Users> findAll();
 }
